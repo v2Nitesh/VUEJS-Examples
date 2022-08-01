481ceda5a56f38e18fac354bc7fd5a265ff9e8f7
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2022-08-01 16:45:39 +0530

    Completed Section 1 with starter project

---
+++ Section_1_Resources/gs-01-starting-project/app.js
@@ -0,0 +1,41 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+
+    },
+    methods: {
+        addGoal() {
+            if (this.enteredValue !== '') {
+                this.goals.push(this.enteredValue)
+                this.enteredValue = ''
+            }
+        }
+    }
+}).mount('#app')
+
+
+
+
+
+// Example in JavaScript
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+
+
+//     const enteredValue = inputEl.value
+//     const listItemEl = document.createElement("li");
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = ''
+// }
+
+// buttonEl.addEventListener('click',addGoal)
+
+
