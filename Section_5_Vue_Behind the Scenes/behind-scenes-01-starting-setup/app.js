ae5e93f0026b17d67cdbc3a3c585364e46ef9874
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2022-08-05 15:32:43 +0530

    Section 5 : Completed - View Life Cycle

---
+++ Section_5_Vue_Behind the Scenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,66 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText)
+    },
+  },
+
+  beforeCreate() {
+    console.log("beforeCreate()")
+  },
+  created() {
+    console.log("created()")
+  },
+  beforeMount() {
+    console.log("beforeMount()")
+  },
+  mounted() {
+    console.log("mounted()")
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()")
+  },
+  updated() {
+    console.log("updated()")
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()")
+
+  },
+  unmounted(){
+    console.log("unmounted()")
+
+  }
+
+});
+
+app.mount('#app');
+
+setTimeout(function() {
+  app.unmount();
+}, 3000)
+
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    };
+  },
+});
+
+
+app2.mount('#app2');
