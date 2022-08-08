6fc1ec4b09b82f0f13bf1b5c9fae7f976c60db67
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2022-08-08 15:46:23 +0530

    Added Good TableView

---
+++ TableExample/vue-table-example/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <TableView/>
+</template>
+
+
+<script>
+import TableView from './components/TableView.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TableView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
